e57e94b02eea6f225a5b773555789bced3e63678
Heydar omid <heydaromid2@gmail.com>
2022-03-11 02:47:20 +0330

    Create footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
-  <footer class="bg-gray-600 h-12 flex items-center">
-      <div class="container mx-auto text-center text-white">footer</div>
+  <footer class="bg-gray-200 h-12 flex items-center">
+      <div class="container mx-auto text-center text-black font-light text-sm">
+          تمامی حقوق مادی و معنوی برای فروشگاه دیجیکالا محفوظ می‌باشد. قدرت گرفته از 
+          <a href="https://landing24.ir" class="text-red-800 hover:text-red-600 font-bold">لندینگ ۲۴</a>
+      </div>
   </footer>
 </template>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header class="container mx-auto mt-6 flex flex-col gap-3">
         <img class="mx-auto" src="https://www.digikala.com/statics/img/svg/logo.svg" width="150">
         <h1 class="text-center text-2xl font-semibold cursor-default text-gray-700" >لیست محصولات فروشگاه دیجیکالا</h1>
-        <input type="text" class="outline-none px-3 py-2 border rounded-lg focus:ring-yellow-200 focus:ring-2 focus:border-yellow-500" 
+        <input type="text" class="outline-none px-3 py-2 border rounded-lg focus:ring-red-200 focus:ring-2 focus:border-red-500" 
         placeholder="نام محصول مورد نظرتان را وارد کنید ...">
     </header>
 </template>
